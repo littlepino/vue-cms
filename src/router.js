649bf0fed3fc25af6b07f2337cf34d164e760390
xfy <793979816@qq.com>
2019-07-03 16:12:04 +0800

    项目第一天打卡

--- src/router.js
+++ src/router.js
@@ -1,10 +1,23 @@
 
+import VueRouter from 'vue-router'
+
+//导入对应的路由组件
+import HomeContainer from './components/tabbar/homeContainer.vue'
+import MemberContainer from './components/tabbar/memberContainer.vue'
+import shopCartContainer from './components/tabbar/shopCarContainer.vue'
+import SearchContainer from './components/tabbar/searchContainer.vue'
 
 //.创建路由对象
 var router = new VueRouter({
     routes: [
+        { path: '/',redirect: '/home' },
+        { path: '/home',component: HomeContainer },
+        { path: '/member',component: MemberContainer },
+        { path: '/shopcar',component: shopCartContainer },
+        { path: '/search',component: SearchContainer }
         
-    ]
+    ],
+    linkActiveClass: 'mui-active'//覆盖默认的路由高亮的类，默认的类名为:router-link-active
 })
 
 export default router
